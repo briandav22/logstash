eb2392c0e0707ee6d09042c53d7ca8aab308b39b
Brian <briandav22@gmail.com>
2019-08-15 10:09:05 -0400

    getting rid of defaults

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from ScrutRequester import ReportAPI, Requester
 from ScrutExporter import Exporter
 
 report_params = ReportAPI()
-logstash_server = "http://127.0.0.1:8080"
+logstash_server = "http://your_logstash:8080"
 report_params.report_options(
-    filters={"sdfDips_0": "in_0A010104_0A010104-11",
+    filters={"sdfDips_0": "in_GROUP_ALL",
             "sdfIPGroups_0": "ex_16800000_src"}, 
     reportTypeLang="srcHosts")
 
@@ -15,8 +15,8 @@ report_params.report_direction(
 report_params.make_object()
 
 scrut_requester = Requester(
-    authToken="1K0tMEqLUaesIeLmvdxWX_F3", 
-    hostname="scrutinizer.plxr.local")
+    authToken="scrutinizerAPIToken", 
+    hostname="ScrutinizerHostname or IP")
 
 print("Getting Scrutinizer Data")    
 data = scrut_requester.make_request(report_params.params)
